e153760da7e7c67c95e1438e0003af18316a3011
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-08 13:53:56 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config her
-  output: 'standalone',
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
