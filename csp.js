23357512a0641e8d5aee1555abad7feb51266866
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-09 17:26:13 +0200

    added csp

---
+++ csp.js
@@ -0,0 +1,37 @@
+const policies = {
+  'default-src': ["'self'"],
+  'script-src': [
+    "'self'",
+    "'unsafe-inline'",
+    "'unsafe-eval'",
+    'https://checkout.stripe.com',
+    'https://js.stripe.com',
+    'https://maps.googleapis.com',
+    'http://vue-frontend-wjofai-d643e5-168-119-233-159.traefik.me',
+  ],
+  'child-src': ["'self'"],
+  'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
+  'img-src': ["'self'", 'https://*.stripe.com', 'https://raw.githubusercontent.com'],
+  'font-src': ["'self'"],
+  'frame-src': [
+    "'self'",
+    'https://checkout.stripe.com',
+    'https://js.stripe.com',
+    'https://hooks.stripe.com',
+  ],
+  'connect-src': [
+    "'self'",
+    'https://checkout.stripe.com',
+    'https://api.stripe.com',
+    'https://maps.googleapis.com',
+  ],
+}
+
+module.exports = Object.entries(policies)
+  .map(([key, value]) => {
+    if (Array.isArray(value)) {
+      return `${key} ${value.join(' ')}`
+    }
+    return ''
+  })
+  .join('; ')
